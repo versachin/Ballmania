720a69d6fce416c32dfacf25655b900016f700ae
Kew Forest Teacher <daronson@kewforest.org>
2018-01-12 05:45:08 -0800

    my name is taneil i like cheeese

--- grotto.py
+++ grotto.py
@@ -3,13 +3,19 @@ import turtle
 wn=turtle.Screen()
 wn.bgcolor("lightblue")
 grotto=turtle.Turtle()
+maze=turtle.Turtle()
+
 obstacle = (0,100)
+grotto.penup()
+grotto.left(90)
+grotto.forward(100)
+
 
 def moveForward():
-    grotto.forward(50)
+    grotto.forward(10)
     if round(grotto.xcor(),0) == obstacle[0] and round(grotto.ycor(),0) == obstacle[1]:
         print("crash")
-        grotto.write("Crash!")
+        grotto.write("CRASH!")
 def turnLeft():
     grotto.left(90)
     
@@ -20,7 +26,8 @@ def quitGame():
     wn.bye()
     
 def move50(x,y):
-    grotto.forward(50)
+    grotto.forward(10)
+
     
 wn.onkey(moveForward, "Up")
 wn.onkey(turnLeft, "Left")
@@ -28,6 +35,20 @@ wn.onkey(turnRight, "Right")
 wn.onkey(quitGame, "q")
 wn.textinput("NIM", "Name of first player:")
 grotto.onclick(move50)
-
+def maze_func(turt):
+    turt.right(90)
+    turt.forward(200)
+    turt.left(90)
+    turt.forward(300)
+    turt.penup()
+    turt.forward(37.5)
+    turt.pendown()
+    turt.left(90)
+    turt.forward(200)
+    turt.left(90)
+    turt.forward(275)
+    turt.penup()
+    turt.forward(25)
+maze_func(maze)
 wn.listen()
 wn.mainloop()
