3b1f61b7bd339c7a4343b8448a24a0406fd0b6d3
Sachin Palta <sachinpalta@gmail.com>
2018-03-05 17:36:29 -0500

    tried to add pygame... and failed miserably

--- grotto.py
+++ grotto.py
@@ -1,86 +1,90 @@
-import turtle
+#import turtle
+import pygame
 
-wn=turtle.Screen()
-wn.bgcolor("lightblue")
-grotto=turtle.Turtle()
-maze=turtle.Turtle()
+#wn=turtle.Screen()
+#wn.bgcolor("lightblue")
+#grotto=turtle.Turtle()
+#maze=turtle.Turtle()
 
-obstacle = (0.100)
-grotto.penup()
-grotto.left(90)
-grotto.forward(100)
-
-
-def moveForward():
-    grotto.forward(10)
-    if round(grotto.xcor(),0) == obstacle[0] and round(grotto.ycor(),0) == obstacle[1]:
-        print("crash")
-        grotto.write("CRASH!")
-def turnLeft():
-    grotto.left(90)
+class mazerunner(pygame.sprite.Sprite):
     
-def turnRight():
-    grotto.right(90)
+
+
+
+
+
+#obstacle = (0.100)
+#grotto.penup()
+#grotto.left(90)
+#grotto.forward(100)
+
+
+
+#def moveForward():
+ #   grotto.forward(10)
+  #  if round(grotto.xcor(),0) == obstacle[0] and round(grotto.ycor(),0) == obstacle[1]:
+   #     print("crash")
+    #    grotto.write("CRASH!")
+#def turnLeft():
+ #   grotto.left(90)
+  #
+#def turnRight():
+ #   grotto.right(90)
     
-def quitGame():
-    wn.bye()
+#def quitGame():
+ #   wn.bye()
     
-def move50(x,y):
-    grotto.forward(10)
+#def move50(x,y):
+ #   grotto.forward(10)
 
     
-wn.onkey(moveForward, "Up")
-wn.onkey(turnLeft, "Left")
-wn.onkey(turnRight, "Right")
-wn.onkey(quitGame, "q")
-wn.textinput("NIM", "Name of first player:")
-grotto.onclick(move50)
-def maze_func(turt):
-    turt.right(90)
-    turt.forward(200)
-    turt.left(90)
-    turt.forward(300)
-    turt.penup()
-    turt.forward(37.5)
-    turt.pendown()
-    turt.left(90)
-    turt.forward(200)
-    turt.left(90)
-    turt.forward(275)
-    turt.penup()
-    turt.forward(25)
-    turt.left(90)
-    turt.pendown()
-    turt.forward(175)
-    turt.left(90)
-    turt.forward(275)
-def maze_two(turt):
-    turt.penup()
-    turt.setpos(300,300)
-    turt.pendown()
-    turt.right(180)
-    turt.forward(500)
-    turt.left(90)
-    turt.forward(250)
-    turt.forward(50)
-    turt.pendown()
-    turt.left(90)
-    turt.forward(500)
-    turt.left(90)
-    turt.forward(250)
-    turt.penup()
-    turt.forward(50)
-    turt.pendown()
-    turt.left(90)
-    turt.forward(260)
-    turt.left(90)
-    turt.forward(200)
-    turt.left(90)
-    turt.forward(50)
-    turt.left(90)
-    turt.forward(50)
+#wn.onkey(moveForward, "Up")
+#wn.onkey(turnLeft, "Left")
+#wn.onkey(turnRight, "Right")
+#wn.onkey(quitGame, "q")
+#wn.textinput("NIM", "Name of first player:")
+#grotto.onclick(move50)
+#def maze_func(turt):
+ #   turt.right(90)
+  ###turt.forward(300)
+    #turt.penup()
+    #turt.forward(37.5)
+#    turt.pendown()
+###  turt.left(90)
+#    turt.forward(275)
+ #   turt.penup()
+  #  turt.forward(25)
+   # turt.left(90)
+    #turt.pendown()
+#    turt.forward(175)
+ #   turt.left(90)
+  #  turt.forward(275)
+#def maze_two(turt):
+ #   turt.penup()
+  #  turt.setpos(300,300)
+   # turt.pendown()
+#    turt.right(180)
+ #   turt.forward(500)
+  #  turt.left(90)
+   # turt.forward(250)
+#    turt.forward(50)
+ ##  turt.left(90)
+    #     turt.forward(500)
+  #  turt.left(90)
+   # turt.forward(250)
+ #   turt.penup()
+  #  turt.forward(50)
+   # turt.pendown()
+    #turt.left(90)
+#    turt.forward(260)
+ #   turt.left(90)
+  #  turt.forward(200)
+   # turt.left(90)
+  #  turt.forward(50)
+ #   turt.left(90)
+   # turt.forward(50)
 
-def make_square(turt,size,x,y):
+"""def make_square(turt,size,x,y):
     turt.penup()
     turt.setpos(x,y)
     turt.pendown()
@@ -90,4 +94,4 @@ def make_square(turt,size,x,y):
 make_square(maze,50,100,200)
 #maze_func(maze)
 wn.listen()
-wn.mainloop()
+wn.mainloop()"""
