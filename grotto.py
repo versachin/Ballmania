caf81088aa31400a57d5f591eaeee359249f5db0
Sachin Palta <sachinpalta@gmail.com>
2018-04-16 09:14:34 -0400

    messed something up

--- grotto.py
+++ grotto.py
@@ -2,8 +2,9 @@
 import pygame
 
 pygame.init()
-
-gameDisplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('MAZERUNNER')
 clock = pygame.time.Clock()
 
@@ -12,56 +13,52 @@ playerrect = player.get_rect()
 
 crashed = False
 
-
-while not crashed:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        print(event)
-    gameDisplay.blit(player, playerrect)
-    pygame.display.update()
-    clock.tick(60)
-
-
-    def game_loop():
-        global paused
-
-        x = (display_width * 0.45)
-        y = (display_height * 0.8)
-
-        x_change = 0
+def move_ball(x, y):
+    gameDisplay.blit(playerrect, (x, y))
 
 
 
 
+def game_loop():
+    global paused
 
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
 
+    x_change = 0
 
+    while not crashed:
 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            x_change = -5
-            pygame.display.update()
-            gameDisplay.blit(player, playerrect)
-        if event.key == pygame.K_RIGHT:
-            x_change = 5
-            pygame.display.update()
-            gameDisplay.blit(player, playerrect)
-        if event.key == pygame.K_p:
-            paused = True
-            pause()
-            pygame.display.update()
-            gameDisplay.blit(player, playerrect)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+            print(event)
+        gameDisplay.blit(player, playerrect)
+        pygame.display.update()
+        clock.tick(60)
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+                pygame.display.update()
+                gameDisplay.blit(player, playerrect)
+            if event.key == pygame.K_RIGHT:
+                x_change = 5
+                pygame.display.update()
+                gameDisplay.blit(player, playerrect)
+            if event.key == pygame.K_p:
+                paused = True
+                pause()
+                pygame.display.update()
+                gameDisplay.blit(player, playerrect)
 
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            x_change = 0
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
 
-
-#x += x_change
-#gameDisplay.fill(white)
+        x += x_change
+        gameDisplay.fill(white)
+        move_ball(x,y)
 
 
 pygame.quit()
