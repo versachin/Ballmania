b7e377f5cf5d2d58afd03ea7afbb74261c361787
Sachin Palta <sachinpalta@gmail.com>
2018-04-16 09:27:21 -0400

    woohooo made ball move on screen yeah so cool

--- grotto.py
+++ grotto.py
@@ -2,6 +2,7 @@
 import pygame
 
 pygame.init()
+white = (0, 200, 200)
 display_width = 800
 display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
@@ -14,18 +15,20 @@ playerrect = player.get_rect()
 crashed = False
 
 def move_ball(x, y):
-    gameDisplay.blit(playerrect, (x, y))
+    gameDisplay.blit(player, (x, y))
 
 
 
 
 def game_loop():
     global paused
+    global crashed
 
     x = (display_width * 0.45)
     y = (display_height * 0.8)
 
     x_change = 0
+    y_change = 0
 
     while not crashed:
 
@@ -44,6 +47,10 @@ def game_loop():
                 gameDisplay.blit(player, playerrect)
             if event.key == pygame.K_RIGHT:
                 x_change = 5
+            if event.key == pygame.K_UP:
+                    y_change = -5
+            if event.key == pygame.K_DOWN:
+                y_change = 5
                 pygame.display.update()
                 gameDisplay.blit(player, playerrect)
             if event.key == pygame.K_p:
@@ -55,12 +62,18 @@ def game_loop():
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_change = 0
 
         x += x_change
+        y += y_change
         gameDisplay.fill(white)
         move_ball(x,y)
+        pygame.display.update()
+        clock.tick(60)
 
-
+game_loop()
 pygame.quit()
 quit()
 
